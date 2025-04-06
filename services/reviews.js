0b6ef58037b390686194eff2278f6fee4083138e
00019170 <00019170aa@gmail.com>
2025-04-06 14:13:25 +0500

    Update reviews.js

--- services/reviews.js
+++ services/reviews.js
@@ -7,7 +7,7 @@ module.exports = {
     
     if (!book) throw new Error('Book not found');
     
-    // Используем Array.isArray вместо util.isArray
+    
     if (!Array.isArray(book.reviews)) {
       book.reviews = [];
     }
@@ -21,5 +21,18 @@ module.exports = {
     book.reviews.push(newReview);
     await writeData(books);
     return newReview;
+  },
+  deleteReview: async (bookId, reviewId) => {
+    const books = await readData();
+    const bookIndex = books.findIndex(b => b.id === bookId);
+    
+    if (bookIndex === -1) throw new Error('Book not found');
+    
+    const reviewIndex = books[bookIndex].reviews.findIndex(r => r.id === reviewId);
+    if (reviewIndex === -1) return false;
+    
+    books[bookIndex].reviews.splice(reviewIndex, 1);
+    await writeData(books);
+    return true;
   }
 };
